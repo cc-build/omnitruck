f4f574813976746b591b11f9b0809f8975c5a8b9
Lance Albertson <lance@osuosl.org>
2021-08-04 12:00:14 -0700

    Update SLES_PROJECT_VERSIONS to use cinc names
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- lib/dist.rb
+++ lib/dist.rb
@@ -7,11 +7,11 @@ module OmnitruckDist
   # need to be remapped to EL before a certain version.
   SLES_PROJECT_VERSIONS = {
     "automate" => "0.8.5",
-    "chef" => "12.21.1",
+    "cinc" => "12.21.1",
     "angrychef" => "12.21.1",
-    "chef-server" => "12.14.0",
+    "cinc-server" => "12.14.0",
     "chefdk" => "1.3.43",
-    "inspec" => "1.20.0",
+    "cinc-auditor" => "1.20.0",
     "angry-omnibus-toolchain" => "1.1.66",
     "omnibus-toolchain" => "1.1.66",
   }.freeze
